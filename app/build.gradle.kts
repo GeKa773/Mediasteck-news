8078e22ac3ac614b1f870ed7799fc3677ba3c3c8
GeKaR <radchenko.kotlin.bsx@gmail.com>
2022-06-24 12:53:58 +0300

    added dependencies commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs.kotlin")
+    //for java
+    //id("androidx.navigation.safeargs")
 }
 
 android {
@@ -29,8 +32,29 @@ android {
 
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    //Избегание включения инфраструктуры отладки в результирующий APK
+    packagingOptions {
+        resources.excludes += "DebugProbesKt.bin"
+    }
 
 }
+
+val daggerVersion = "2.40.5"
+val coroutinesVersion = "1.6.3"
+val roomVersion = "2.4.2"
+val retrofitVersion = "2.9.0"
+val retrofitAdapterVersion = "0.9.2"
+val archComponentVersion = "2.4.1"
+val navVersion = "2.4.2"
 
 dependencies {
 
@@ -45,6 +69,45 @@ dependencies {
 
 //dagger hilt
 dependencies {
-    implementation("com.google.dagger:hilt-android:2.40.5")
-    kapt("com.google.dagger:hilt-compiler:2.40.5")
+    implementation("com.google.dagger:hilt-android:$daggerVersion")
+    kapt("com.google.dagger:hilt-compiler:$daggerVersion")
+}
+
+//coroutines
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+}
+
+//room
+dependencies {
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+}
+
+//retrofit
+dependencies {
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:$retrofitAdapterVersion")
+}
+
+//lifecycle
+dependencies {
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$archComponentVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$archComponentVersion")
+    implementation("androidx.lifecycle:lifecycle-common-java8:$archComponentVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$archComponentVersion")
+
+//    implementation("android.arch.lifecycle:extensions:1.1.1")
+//    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+//    implementation("androidx.fragment:fragment-ktx:1.4.1")
+}
+
+//navigation
+dependencies{
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,17 @@ buildscript {
         mavenCentral()
     }
 
+    val navVersion = "2.4.2"
+
     dependencies {
         classpath ("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
     }
 }
 plugins {
     id ("com.android.application") version("7.2.1")  apply false
     id ("com.android.library") version ("7.2.1") apply false
-    id ("org.jetbrains.kotlin.android") version ("1.6.10") apply false
+    id ("org.jetbrains.kotlin.android") version ("1.6.21") apply false
 }
 
 
